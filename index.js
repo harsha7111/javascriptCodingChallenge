22c1e48e98f899d7b42e239824915df1e9f84c35
Harsha <68165196+harsha7111@users.noreply.github.com>
2020-07-31 18:56:56 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,10 @@
+function FirstReverse(str) { 
+
+  // code goes here  
+  str = str.split('').reverse().join('');
+  return str; 
+
+}
+   
+// keep this function call here 
+console.log(FirstReverse('Hello World and Coders'));
